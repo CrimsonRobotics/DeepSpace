2e99140cd830059ccc8454aec2ac4610224b9b50
Lauren Kadlec <laurenkadlec@gmail.com>
2019-02-02 12:26:27 -0600

    Added in encoder code. waiting for testing

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,9 +4,11 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.Joystick;
 import frc.robot.commands.ClimberUp;
@@ -20,7 +22,7 @@ public class Climber extends Subsystem {
 	private WPI_TalonSRX climberBackLeft;
 	private WPI_TalonSRX climberFrontRight;
 	private WPI_TalonSRX climberFrontLeft;
-	AnalogInput EncCBR;
+	private Encoder EncoderCBR;
 	private final double INPUT_MULTIPLIER = 1;
 
 	public Climber(int climbIDBL, int climbIDBR, int climbIDFL, int climbIDFR){
@@ -28,20 +30,28 @@ public class Climber extends Subsystem {
 		climberBackLeft = new WPI_TalonSRX(climbIDBL);
 		climberFrontRight = new WPI_TalonSRX(climbIDFR);
 		climberFrontLeft = new WPI_TalonSRX(climbIDFL);
-		EncCBR = new AnalogInput(1);
+		climberBackRight.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 0);
+		climberBackRight.setSensorPhase(true);
 	}
 	@Override
 	protected void initDefaultCommand() {
 		//setDefaultCommand(new ClimberUp());
 	}
 	public void ClimberUp(){
-		while(EncCBR.getValue()>2){
+		if(climberBackRight.getSelectedSensorPosition()<500){
 			climberBackLeft.set(.1);
-		climberFrontLeft.set(.1);
+			climberFrontLeft.set(.1);
 		//opposite direction as other two
-		climberFrontRight.set(-.1);
-		climberBackRight.set(-.1);
+			climberFrontRight.set(-.1);
+			climberBackRight.set(-.1);
 			}
+		else{
+			climberBackLeft.set(0);
+			climberFrontLeft.set(0);
+		//opposite direction as other two
+			climberFrontRight.set(0);
+			climberBackRight.set(0);
+		}
 		}
 	}
 
