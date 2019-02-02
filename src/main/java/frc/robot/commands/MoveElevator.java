0ce978ce034d575491fc404f3c88123a0cef3e85
Stuti Arora <arorastu100@gmail.com>
2019-02-02 10:01:52 -0600

    storing

---
+++ src/main/java/frc/robot/commands/MoveElevator.java
@@ -0,0 +1,39 @@
+package frc.robot.commands;
+
+import frc.robot.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class MoveElevator extends Command {
+
+    public MoveElevator() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.elevator);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.elevator.moveElevator(Robot.m_oi.getcoDriver());
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
